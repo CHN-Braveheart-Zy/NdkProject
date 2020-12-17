2cd6f4ec45d54482c411a8cb32aa9af0a5aa0d8d
ZhangJunPu <694733055@qq.com>
2020-12-17 22:09:05 +0800

    first commit

---
+++ app/src/main/java/com/zjp/ndkproject/screen_live/ScreenLive.java
@@ -0,0 +1,44 @@
+package com.zjp.ndkproject.screen_live;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.AsyncTask;
+
+import androidx.annotation.Nullable;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static android.app.Activity.RESULT_OK;
+
+public class ScreenLive implements Runnable{
+    public static final int REQUEST_CODE = 0X01;
+    private MediaProjectionManager projectionManager;
+    private MediaProjection mediaProjection;
+
+    public void startLive(String url, ScreenLiveActivity activity) {
+        projectionManager = (MediaProjectionManager) activity.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        Intent screenCaptureIntent = projectionManager.createScreenCaptureIntent();
+        activity.startActivityForResult(screenCaptureIntent, REQUEST_CODE);
+    }
+
+    public void stopLive() {
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            mediaProjection = projectionManager.getMediaProjection(resultCode, data);
+            //mediaProjection.createVirtualDisplay()
+            //连接rtmp服务器
+            //获取数据编码
+            AsyncTask.THREAD_POOL_EXECUTOR.execute(this);
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ app/src/main/java/com/zjp/ndkproject/screen_live/ScreenLiveActivity.java
@@ -0,0 +1,37 @@
+package com.zjp.ndkproject.screen_live;
+
+import android.content.Intent;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zjp.ndkproject.R;
+
+public class ScreenLiveActivity extends AppCompatActivity {
+
+    private ScreenLive screenLive;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_live);
+        screenLive = new ScreenLive();
+    }
+
+    public void startLive(View view) {
+        screenLive.startLive("", this);
+    }
+
+    public void stopLive(View view) {
+        screenLive.stopLive();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        screenLive.onActivityResult(requestCode, resultCode, data);
+    }
+}
